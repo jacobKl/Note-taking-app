c083b0094d25e9b94c7c68f5c2e0f096f76de728
Kuba <kqklimek@gmail.com>
2020-05-12 11:20:03 +0200

    Updates

---
+++ precache-manifest.3554f31a484b6da5f434aeb78b5ec9f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd14b4b77180cd0ab1c966b4ce2ef3d7",
+    "url": "/Note-taking-app/index.html"
+  },
+  {
+    "revision": "004a29babe614aa32e72",
+    "url": "/Note-taking-app/static/css/main.77e120dc.chunk.css"
+  },
+  {
+    "revision": "98ddccb5e341887ddba9",
+    "url": "/Note-taking-app/static/js/2.f12c62ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Note-taking-app/static/js/2.f12c62ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "004a29babe614aa32e72",
+    "url": "/Note-taking-app/static/js/main.fe6713dc.chunk.js"
+  },
+  {
+    "revision": "4725755f21cc602798a0",
+    "url": "/Note-taking-app/static/js/runtime-main.5e480ef9.js"
+  }
+]);
